f5c5f7c22c9dc9da0ef55109cd15c28ab6211a6c
1415003719 <1415003719@qq.com>
2016-09-30 18:54:14 +0800

    del

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-﻿var info=getInfo();
 var flag=1;
+var info=getInfo();
 $(function(){
   var p=0;
   $('.position').html('第'+flag+'页/共'+Math.ceil(info.length/5)+'页');
   for(var i=0;i<info.length;i++){
     var art="<article class='art' id="+i+">"+
-    "<h3><a href='https://1415003719.github.io/zhx.github.com/article_html/article_1.html?id=1'>"+info[i].title+"</a></h3>"+
+    "<h3><a href='../article_html/article_1.html?id=1'>"+info[i].title+"</a></h3>"+
     "<p>"+info[i].details+"</p>"+
     "<div><span><i class='glyphicon glyphicon-time'></i><time class='time'>"+info[i].time+"</time></span></div></article>";
       $('.navgation').before(art);
@@ -51,7 +51,7 @@ $(function(){
 function getInfo(){
   var info;
   $.ajax({
-    url:'https://1415003719.github.io/zhx.github.com/info/info.json',
+    url:'../info/info.json',
     type:'get',
     jsonp:'callback',
     dataType:'json',
